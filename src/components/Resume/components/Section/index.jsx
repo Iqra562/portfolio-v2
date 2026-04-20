c6e9d0e71853acb2cd3f926972026a4379cbaef2
Iqra Khaliq <114660239+Iqra562@users.noreply.github.com>
2026-04-20 11:34:50 +0500

    Update index.jsx

--- src/components/Resume/components/Section/index.jsx
+++ src/components/Resume/components/Section/index.jsx
@@ -11,22 +11,7 @@ function Section() {
            
             
            <div className="pt-0 md:pt-5 space-y-5 md:space-y-0">
-             <div className="flex flex-col md:flex-row gap-y-4 ">
-                <div className=" md:w-[30%]   flex flex-col itemscenter space-x2 md:space-x-0  md:block">
-                  <h6 className="text-white text-lg">Silicon Systems</h6>
-                  <span className="text-primary">Nov 2024 - March 2025</span>
-                </div>
-                <div className="relative  border-l-[1px] border-night-black  pb-4 md:pb-8 md:w-[70%]  " >
-                   <span className="bg-primary/20 absolute -left-[6.5px] md:-left-[13px]  -top-[3.5px] md:-top-[10px] h-3 md:h-[25px] w-3 md:w-[25px] rounded-full hidden md:flex items-center justify-center  ">
-  <span className="bg-secondary/60 h-1 md:h-[10px] w-1 md:w-[10px] rounded-full"></span>
-</span>
-<div className=" pl-2 md:pl-8  ">
-
-                  <h6 className="text-2xl text-white font-semibold  -mt-1">Frontend Internee</h6>
-                  <p className="text-primary">Built  responsive web interfaces using React.</p>
-</div>
-                </div>
-             </div>
+             
              <div className="flex flex-col md:flex-row gap-y-4">
                 <div className=" md:w-[30%]   flex items-center space-x-2 md:space-x-0  md:block">
                   <h6 className="text-white text-lg">Apptalyst</h6>
